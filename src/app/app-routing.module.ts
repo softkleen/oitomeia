de8f600977a9767add41a70cc0b2bced04e818fc
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos4@senacsp.edu.br>
2021-02-02 14:12:18 -0300

    inicio tela user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
   }
 ];
 
