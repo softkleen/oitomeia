248ac7fef1e806c998118cfd13b735eed04697ce
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos4@senacsp.edu.br>
2021-02-09 15:11:05 -0300

    adicionando usuário ao banco

--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -1,3 +1,5 @@
+import { promise } from 'protractor';
+import { PostService } from './../../../services/post.service';
 import { async } from '@angular/core/testing';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -12,9 +14,14 @@ export class UserPage implements OnInit {
 nome:string;
 email:string;
 senha:string;
+nivel:string;
 users:any[]=[];
 
-  constructor(private route: Router, private toastCtrl: ToastController) { 
+  constructor(
+    private route: Router, 
+    private toastCtrl: ToastController,
+    private service:PostService
+    ) { 
     let usersJson = localStorage.getItem('userDb');
     //console.log(tarefasJson);
     if (usersJson!=null) {
@@ -25,24 +32,41 @@ users:any[]=[];
   ngOnInit() {
   }
  async salvar(){
-
-   let user = {nome:this.nome,email:this.email,senha:this.senha};
-   this.users.push(user);
-   this.atualizaLocalStorage();
-   this.route.navigate(['list-users']);
-   this.nome='';
-   this.email='';
-   this.senha='';
-
-   const toast = await this.toastCtrl.create({
-     message:"Usuário gravado com sucesso",
-     duration:2000,
-     color:"success"
-   });
-   toast.present();
-
-   console.log(this.users);
- }
+   //let user = {nome:this.nome,email:this.email,senha:this.senha};
+   //this.users.push(user);
+    return new Promise(res => {
+      let dados = {
+        requisicao:"add",
+        nome:this.nome,
+        email:this.email,
+        senha:this.senha,
+        nivel: this.nivel
+      };
+      this.service.dadosApi(dados,'apiuser.php').subscribe(async data => {
+        if(data['success']){
+          //debugger;
+          const toast = await this.toastCtrl.create({
+            message:"Usuário gravado com sucesso",
+            duration:2000,
+            color:"success"
+          });
+          await toast.present();
+          this.route.navigate(['list-users']);
+          this.nome='';
+          this.email='';
+          this.senha='';
+          this.nivel = '';
+        }else{
+          const toast1 = await this.toastCtrl.create({
+            message:data['msg'],
+            duration:2000,
+            color:"warning"
+          });
+          await toast1.present();
+        }
+      });
+    });   
+ } // final do método salvar
  cancelar(){
    
  }
